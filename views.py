ab6e3e87b12790a4d999fe456ac96853ec3cca6f
chiyu <rhwang3313@gmail.com>
2025-10-17 22:10:56 +0800

    创建视图login

--- views.py
+++ views.py
@@ -5,7 +5,10 @@
 # File: views.py
 # Description: <description>
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
